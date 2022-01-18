09fcb6d1c03311fb63fc14cf77dd5559162d276a
Chriss Desmonds <chimbadzwa.christopherdesmond@gmail.com>
2022-01-18 22:45:21 +0200

    initial project setup

---
+++ pmshs/src/src_code/Config.java
@@ -0,0 +1,150 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package src_code;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author cchimbadzwa
+ */
+public class Config extends JFrame {
+    private File configFile = new File("config.properties");
+	private Properties configProps;
+	
+	private JLabel labelHost = new JLabel("Host name: ");
+	private JLabel labelPort = new JLabel("Port number: ");
+	private JLabel labelUser = new JLabel("Username: ");
+	private JLabel labelPass = new JLabel("Password: ");
+	
+	private JTextField textHost = new JTextField(20);
+	private JTextField textPort = new JTextField(20);
+	private JTextField textUser = new JTextField(20);
+	private JTextField textPass = new JTextField(20);
+	
+	private JButton buttonSave = new JButton("Save");
+	
+	public Config() {
+		super("Local Database Properties Configuration");
+		setLayout(new GridBagLayout());
+		GridBagConstraints constraints = new GridBagConstraints();
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+		constraints.insets = new Insets(10, 10, 5, 10);
+		constraints.anchor = GridBagConstraints.WEST;
+		
+		add(labelHost, constraints);
+		
+		constraints.gridx = 1;
+		add(textHost, constraints);
+		
+		constraints.gridy = 1;
+		constraints.gridx = 0;
+		add(labelPort, constraints);
+		
+		constraints.gridx = 1;
+		add(textPort, constraints);
+
+		constraints.gridy = 2;
+		constraints.gridx = 0;
+		add(labelUser, constraints);
+		
+		constraints.gridx = 1;
+		add(textUser, constraints);
+
+		constraints.gridy = 3;
+		constraints.gridx = 0;
+		add(labelPass, constraints);
+		
+		constraints.gridx = 1;
+		add(textPass, constraints);
+		
+		constraints.gridy = 4;
+		constraints.gridx = 0;
+		constraints.gridwidth = 2;
+		constraints.anchor = GridBagConstraints.CENTER;
+		add(buttonSave, constraints);
+		
+		buttonSave.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					saveProperties();
+					JOptionPane.showMessageDialog(Config.this, 
+							"Properties were saved successfully!");	
+                                        dispose();
+                                        DBConnect.createCon();
+				} catch (IOException ex) {
+					JOptionPane.showMessageDialog(Config.this, 
+							"Error saving properties file: " + ex.getMessage());		
+				}
+			}
+		});
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		pack();
+		setLocationRelativeTo(null);
+		setVisible(true);
+		
+		try {
+			loadProperties();
+		} catch (IOException ex) {
+			JOptionPane.showMessageDialog(this, "The Database Connection cannot be established.\nConfigure the Database configurations.");
+		} 
+	}
+
+	private void loadProperties() throws IOException {
+		Properties defaultProps = new Properties();
+		// sets default properties
+		defaultProps.setProperty("host", "");
+		defaultProps.setProperty("port", "");
+		defaultProps.setProperty("user", "");
+		defaultProps.setProperty("pass", "");
+		
+		configProps = new Properties(defaultProps);
+		
+		// loads properties from file
+		InputStream inputStream = new FileInputStream(configFile);
+		configProps.load(inputStream);
+		inputStream.close();
+	}
+	
+	private void saveProperties() throws IOException {
+		configProps.setProperty("host", textHost.getText());
+		configProps.setProperty("port", textPort.getText());
+		configProps.setProperty("user", textUser.getText());
+		configProps.setProperty("pass", textPass.getText());
+		OutputStream outputStream = new FileOutputStream(configFile);
+		configProps.store(outputStream, "host setttings");
+		outputStream.close();
+	}
+	
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new Config();
+			}
+		});
+	}
+}
---
+++ pmshs/src/src_code/DBConnect.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package src_code;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author cchimbadzwa
+ */
+public class DBConnect {
+    private static Connection con;
+    private static String port, uname, pwd;
+    private static boolean read;
+    
+    public static Connection getConnection(){
+        if(con == null){
+            JOptionPane.showMessageDialog(null, "No database connection found.", "ERROR", 0);
+        }
+        
+        return con;
+    }
+    
+    public static boolean readSettings() throws IOException {
+        // loads properties from file
+        File configFile = new File("config.properties");
+        try {
+            FileReader reader = new FileReader(configFile);
+            Properties props = new Properties();
+            props.load(reader);
+           
+            String host = props.getProperty("host");
+            port = props.getProperty("port");
+            uname = props.getProperty("user");
+            if (props.getProperty("pass") == null) {
+                pwd = "";
+            } else {
+                pwd = props.getProperty("pass");
+            }
+
+            System.out.println( host + " " + port + " " + uname + " " + pwd);
+            reader.close();
+            
+            if(port.equals("") || uname.equals("")){
+                //re = true;
+                return false;
+            }
+            else{
+                return true; 
+            }
+            
+        } catch (FileNotFoundException ex) {
+            System.out.println("File Not Found with Error: " + ex);
+            return false;
+        } catch (IOException ex) {
+            System.out.print("Error is: " + ex);
+            return false;
+        }
+    }
+    
+    public static boolean createCon() throws IOException {        
+//        try{
+//            read = readSettings();
+//        } 
+//        catch (IOException ex) {
+//            JOptionPane.showMessageDialog(null, "Error: " + ex);
+//        }
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:" + port + "/pmshsm", uname, pwd);
+            System.out.println("Connection established......");
+            return true;
+        } catch (Exception ex) {
+            System.out.println("Error: " + ex);
+            JOptionPane.showMessageDialog(null, "Error: " + ex);
+            return false;
+        }
+    }
+    
+}
